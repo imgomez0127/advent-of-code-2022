e391069ee350194a4c84d8a328f7121e8f8d7ba9
imgomez0127 <ianm0127@gmail.com>
2022-12-01 23:49:45 -0500

    Edited day 1 to use quickselect for O(n) runtime overall

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -13,14 +13,47 @@ fn parse1(input: &str) -> Vec<Vec<i32>> {
     meals.map(parse_calories).collect()
 }
 
+fn lomuto_partition(vector: &mut Vec<i32>, lo: usize, hi:usize) -> usize {
+    let pivot = vector[hi];
+    let mut i = lo;
+    for j in lo..hi {
+        if vector[j] < pivot {
+            i += 1;
+            vector.swap(i-1, j);
+        }
+    }
+    i += 1;
+    vector.swap(i-1, hi);
+    i-1
+}
+
+fn partition_vec(vector: &mut Vec<i32>, lo:usize, hi:usize, k: usize) {
+    if lo > hi {
+        return;
+    }
+    let pivot = lomuto_partition(vector, lo, hi);
+    if pivot == vector.len() - k {
+        return;
+    }
+    if pivot < vector.len() - k {
+        partition_vec(vector, pivot+1, hi, k);
+    }
+    else {
+        partition_vec(vector, lo, pivot-1, k)
+    }
+}
+
 fn task(meals: Vec<Vec<i32>>, k: usize) -> i32 {
-    let mut calories : Vec<i32> = meals.into_iter()
-                                       .map(|meal: Vec<i32>| meal.iter().sum::<i32>()).collect();
-    calories.sort_unstable();
+    let mut calories : Vec<i32> = meals
+        .iter()
+        .cloned()
+        .map(|meal: Vec<i32>| meal.iter().sum::<i32>())
+        .collect();
+    let size = calories.len() - 1;
+    partition_vec(&mut calories, 0, size, k);
     calories.reverse();
     calories[..k].iter().sum()
 }
-
 
 fn main() {
     let input = fs::read_to_string("input.txt").unwrap();
